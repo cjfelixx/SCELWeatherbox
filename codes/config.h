6b38c1e1fa7ba9cf7fe677349c1072d8112a6df6
RAMMELL TRANSFIGURACION <35291551+rammell@users.noreply.github.com>
2018-04-10 12:40:02 -1000

    Update config.h

--- codes/config.h
+++ codes/config.h
@@ -3,15 +3,15 @@
 /* Sensor On/Off */
 #define PIN_SEN_EN 5
 
-/* Humidity */
-#define PIN_HUMID_CLK 7
-#define PIN_HUMID_DATA 8
+/* BME 280 */
+/*BME 280~ humidity temp pressure*/
+#define BME_SCK 13
+#define BME_MISO 12
+#define BME_MOSI 11
+ #define  BME_CS 10 
 
 /* Solar Irradiance */
 #define PIN_APOGEE_V A2
-
-/* Temperature */
-#define PIN_ROOF_TEMP 3
 
 /* Panel and Battery Voltage */
 #define PIN_SOLAR_V A1
@@ -25,7 +25,3 @@
 /* Receiver/Transmitter */
 #define PIN_RX 0
 #define PIN_TX 1
-
- /* I2C/Pressure */
- #define _PIN_SDA A4
- #define _PIN_SCL A5
