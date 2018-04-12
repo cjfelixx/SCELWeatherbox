e6ca577961838ffb4cc224beaec7e540d51c5f4c
RAMMELL TRANSFIGURACION <35291551+rammell@users.noreply.github.com>
2018-04-12 12:21:29 -1000

    Create routine.cpp

---
+++ routine.cpp
@@ -0,0 +1,19 @@
+#include "routine.h"
+
+void routine(int *count)
+{
+  long wait_ms = 1000;
+  unsigned long transmit_timer = millis();
+
+  make_packet(*count);
+
+  (*count)++;
+
+  if((*count) >= 60){
+    send_packet();
+    clear_packet();
+    (*count) = 0;
+    
+  }
+ while ((millis() - transmit_timer) <= wait_ms); 
+}
