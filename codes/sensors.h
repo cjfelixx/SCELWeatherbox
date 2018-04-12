4251d70ec6bb68dbe205bd2af377c6cb8958a500
cjfelixx <35508297+cjfelixx@users.noreply.github.com>
2018-04-12 13:04:27 -1000

    Update sensors.h

--- codes/sensors.h
+++ codes/sensors.h
@@ -1,26 +1,19 @@
-/* File: sensors.h
-   Header file for Sensor functions
-*/
+/* Sensors Libraries*/
+#include <Wire.h>
+#include <XBee.h>
+#include <SPI.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BME280.h>
+#include "Arduino.h"
+#include "SI114X.h"
 
-/* Libraries */
-#include <SHT1x.h>                  /* Humidity Sensor */
-#include <OneWire.h>                /*Temperature Sensor */
-#include <DallasTemperature.h>      /* Temperature Sensor */
-#include <Adafruit_BMP085.h>        /*Pressure Sensor */
-
-/* Create instances of object */
-extern SHT1x sht1x;
-extern OneWire oneWire;
-extern DallasTemperature dallas_roof_sen;
-extern Adafruit_BMP085 bmp085;
-
-/*Initialization Function */
+/*Initialization Function*/
 void sensor_init(void);
 
-/* Sensor Functions */
-long sensorPressure(void);
-long sensorSolarIrrd(void);
-long sensorHumidity(void);         
-long sensorRoofTempdecic(void);             
-long sensorBattery(void);
-long sensorPanelmV(void);
+/*Sensor Functions*/
+long sPressure(void);
+long sSolIrrad(void);
+long sHumidity(void);
+long sTemperature(void);
+long batteryStatus(void);
+long panelStatus(void);
