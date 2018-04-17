ecd39df57740eeeb1b9ed26ca00a84b922d83639
cjfelixx <35508297+cjfelixx@users.noreply.github.com>
2018-04-17 12:43:29 -1000

    Update weatherbox.ino

--- codes/weatherbox.ino
+++ codes/weatherbox.ino
@@ -5,18 +5,17 @@
 
 /* Libraries */
 #include "config.h"
-#include "sensor.h"
+#include "sensors.h"
 #include "transmit.h"
 #include "schema.h"
 #include "routine.h"
+#include "SI114X.h"
 
 /* External Libraries */
-#include <SHT1x.h>
 #include <OneWire.h>
-#include <DallasTemperature.h>
-#include <Adafruit_BMP085.h>
+#include <Adafruit_BME280.h>
+#include <Adafruit_Sensor.h>
 #include <XBee.h>
-#include <EEPROM.h>
 
 /* Global Variables for Packet */
 schema_1 Gpacket;
@@ -29,16 +28,16 @@ int G_count = 0;
 
 void setup() {
 
-	/* Initialization */
-	sensor_init();
-	Serial.begin(9600);
-	Gxbee.begin(Serial); 
+    /* Initialization */
+    sensor_init();
+    Serial.begin(9600);
+    Gxbee.begin(Serial); 
 
-	/*Packet Initialization */
-	clear_Packet();
+    /*Packet Initialization */
+    clear_Packet();
 }
 
 
 void loop() {
-	routine(&G_count);
+    routine(&G_count);
 }
