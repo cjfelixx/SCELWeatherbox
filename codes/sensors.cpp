cc97343a7c5e963112544fb4b5d17d92e7e8f4e4
cjfelixx <35508297+cjfelixx@users.noreply.github.com>
2018-04-18 23:10:45 -1000

    Update sensors.cpp

--- codes/sensors.cpp
+++ codes/sensors.cpp
@@ -4,42 +4,52 @@
 #include "SI114X.h"
 
 /*Objects*/
-Adafruit_BME280 bme;
-SI114X SI1145 = SI114X();
+Adafruit_BME280 bme; //I2C wiring
+SI114X SI1145 = SI114X(); 
 
-void sensor_init(void){
+void sensor_init(void)
+{
    bme.begin();
 }
 
-/*Humidity*/
-long sHumidity(void){
+/*Humidity*/ 
+long sHumidity(void)
+{
   long val = bme.readHumidity();
+  return val; //in percentage
 }
 
 /*Pressure*/
-long sPressure(void){
+long sPressure(void)
+{
   long val = bme.readPressure();
+  return val; //unit in hPa
 }
 
-/*Temperature*/
-long sTemperature(void){
+/*Temperature*/ 
+long sTemperature(void)
+{
   long val = bme.readTemperature();
+  return val; //in C
 }
 
 /*Solar Irradiance*/
-long sSolIrrad(void){
+long sSolIrrad(void)
+{
   long val = SI1145.ReadVisible();
-  return (val) //check datasheet for this calc
+  return val; //check datasheet for this calc
 }
 
 /*Battery*/
-long battStatus(void){
+long battStatus(void)
+{
   long val = analogRead(BATT_PIN)*(5000.0/1023);
   return val;
 }
 
 /*SOLAR PANEL*/
-long panelStatus(void){
+long panelStatus(void)
+{
   long val = 2 * (analogRead(PANEL_PIN)*(5000.0/1023);
   return val;
 }
